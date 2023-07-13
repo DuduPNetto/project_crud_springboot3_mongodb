10bec8b7778bd4bb522804fd213b454ac7a42319
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-13 17:14:41 -0300

    @Query search

--- src/main/java/com/eduardonetto/main/repositories/PostRepository.java
+++ src/main/java/com/eduardonetto/main/repositories/PostRepository.java
@@ -3,11 +3,15 @@ package com.eduardonetto.main.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.eduardonetto.main.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
--- src/main/java/com/eduardonetto/main/services/PostService.java
+++ src/main/java/com/eduardonetto/main/services/PostService.java
@@ -49,7 +49,7 @@ public class PostService {
 	}
 
 	public List<Post> findByTitle(String title) {
-		return repository.findByTitleContainingIgnoreCase(title);
+		return repository.searchTitle(title);
 	}
 
 }
