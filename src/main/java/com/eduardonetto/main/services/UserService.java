ecd70b085c9d010ac70bb5c716bd3dddcf359825
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-13 11:23:28 -0300

    User findById and ResourceExceptionHandler

--- src/main/java/com/eduardonetto/main/services/UserService.java
+++ src/main/java/com/eduardonetto/main/services/UserService.java
@@ -1,12 +1,14 @@
 package com.eduardonetto.main.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardonetto.main.entities.User;
 import com.eduardonetto.main.repositories.UserRepository;
+import com.eduardonetto.main.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,14 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Object not found. Id " + id);
+		}
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id " + id));
 	}
 
 }
