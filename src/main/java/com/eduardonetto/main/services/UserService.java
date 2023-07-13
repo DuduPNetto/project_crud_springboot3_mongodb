7fe63af2b00fae9a47910be37e93bfcdbda15625
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-13 12:02:53 -0300

    User insert

--- src/main/java/com/eduardonetto/main/services/UserService.java
+++ src/main/java/com/eduardonetto/main/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eduardonetto.main.dto.UserDTO;
 import com.eduardonetto.main.entities.User;
 import com.eduardonetto.main.repositories.UserRepository;
 import com.eduardonetto.main.services.exception.ObjectNotFoundException;
@@ -26,6 +27,14 @@ public class UserService {
 			throw new ObjectNotFoundException("Object not found. Id " + id);
 		}
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id " + id));
+	}
+
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 
 }
